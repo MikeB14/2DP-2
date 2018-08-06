5d41212ed8b3ef9755115cc30c4126607eae88f8
MikeB14 <41544531+MikeB14@users.noreply.github.com>
2018-08-05 18:51:32 -0700

    CameraShake Started

---
+++ Assets/Scripts/CameraShake.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Animations;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class CameraShake : MonoBehaviour {
+
+	private int ShakeCount = 0;
+	private Animator Anim;
+
+	// Use this for initialization
+	void Start () {
+		Anim = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void Shake(){
+		if(CrossPlatformInputManager.GetButtonDown("Fire1") && ShakeCount == 0){
+		Anim.SetTrigger("CameraShake1");
+	}
+	else if(CrossPlatformInputManager.GetButtonDown("Fire1") && ShakeCount == 1){
+		Anim.Play("CameraShake2");
+	}
+	else if(CrossPlatformInputManager.GetButtonDown("Fire1") && ShakeCount == 2){
+		Anim.Play("CameraShake3");
+	}
+	else if (CrossPlatformInputManager.GetButtonDown("Fire1") && ShakeCount == 3){
+		Anim.Play("CameraShake4");
+		ShakeCount = 0;
+	}
+	else {
+		Anim.Play("CameraIdle");
+	}
+}
+}
+
